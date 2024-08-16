5ccffd881fd37ba98cbf7dc086a2f54371859658
Jhonatan Quiroga <jdquirogape@ufpso.edu.co>
2024-08-16 17:38:57 -0500

    no message

--- rest-api/src/controllers/user.Controller.js
+++ rest-api/src/controllers/user.Controller.js
@@ -1,9 +1,11 @@
+import { User } from '../models/User.js'
+
 export const getUser = (req, res) => {
     res.send('getting users')
 }
 
 export const createUser = (req, res) => {
-    console.log(req.body)
+    const {nombre,usuario,contraseña,documento} = req.body
     res.send('creating users')
 }
 
