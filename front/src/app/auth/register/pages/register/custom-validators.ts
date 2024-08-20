e26f7dc08dad0f2397ba9025e7d8f6e98f46ca7c
Jhonatan Quiroga <jdquirogape@ufpso.edu.co>
2024-08-20 12:08:58 -0500

    no message

---
+++ front/src/app/auth/register/pages/register/custom-validators.ts
@@ -0,0 +1,35 @@
+// import { customPasswordValidator } from './custom-validators';
+
+
+
+import { AbstractControl, FormGroupDirective,NgForm, PatternValidator, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { ErrorStateMatcher } from "@angular/material/core";
+
+export const crossmPasswordMatchingValidator: ValidatorFn = (
+  FormGroupControl: AbstractControl<{contrasena: string; confirmarContrasena: string
+}>
+): ValidationErrors | null => {
+  const contrasena = FormGroupControl.value.contrasena;
+  const confirmarContrasena = FormGroupControl.value.confirmarContrasena
+
+  return contrasena !== confirmarContrasena ? {crossmPasswordError: true} : null;
+}
+
+const patternPassword = new RegExp('')
+
+export const customPasswordValidator = (control:AbstractControl) => {
+  const value = control.value;
+
+  if(!patternPassword.test(value)) {
+   return { customPasswordValidator: true}
+  }
+  return null;
+}
+
+export class PasswordStateMatcher implements ErrorStateMatcher{
+  isErrorState(control: AbstractControl, form: FormGroupDirective | NgForm): boolean {if(!control || !control.parent) {
+    return false;
+  }
+  return control.parent.hasError('crossConfirmPassword');
+  }
+}
--- front/src/app/auth/register/pages/register/register.component.ts
+++ front/src/app/auth/register/pages/register/register.component.ts
@@ -1,14 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Form, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { crossmPasswordMatchingValidator, PasswordStateMatcher } from './custom-validators';
+import { ErrorStateMatcher } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule,ReactiveFormsModule,],
+  imports: [RouterModule,ReactiveFormsModule,MatFormFieldModule,MatInputModule],
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
+
+  PasswordStateMatcher = new PasswordStateMatcher()
 
     private readonly _FormBuilder = inject(FormBuilder);
 
@@ -19,7 +26,8 @@ export class RegisterComponent {
       documento: ['', Validators.required],
       contrasena: ['', Validators.required],
       confirmarContrasena: ['', Validators.required]
-    })
+    }, {validators:crossmPasswordMatchingValidator})
+
 
   clickRegister(): void {
 // console.log(this.formGroup.get('nombre')?.value)
@@ -28,4 +36,27 @@ export class RegisterComponent {
 
     console.log(this.formGroup.controls.nombre.valid)
   }
+
+
+  get namesField (): FormControl{
+    return this.formGroup.controls.nombre
+  }
+
+  get lastNameField(): FormControl{
+    return this.formGroup.controls.apellido
+  }
+  get userField(): FormControl{
+    return this.formGroup.controls.usuario
+  }
+  get documentField(): FormControl{
+    return this.formGroup.controls.documento
+  }
+  get passwordField (): FormControl{
+    return this.formGroup.controls.contrasena
+  }
+  get confirmPasswordField(): FormControl{
+    return this.formGroup.controls.confirmarContrasena
+  }
+
+
 }
